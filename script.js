640ad60e28a3dfc76a0b163e846a39b6692f8e8c
Alex Rock <1675576+nynexman4464@users.noreply.github.com>
2024-02-12 17:57:29 -0500

    primitive text rendering

--- script.js
+++ script.js
@@ -20,12 +20,14 @@ function handleError(err) {
 
 function renderIt(type, blob) {
 	console.log(type, blob);
+	getCanvasContext(true);
 	const prefix = type.split("/");
 	switch (prefix[0]) {
 		case "image":
 			renderImage(blob);
 			break;
 		case "text":
+			renderText(blob);
 			break;
 		default:
 			handleError("Weird type: " + type);
@@ -33,7 +35,7 @@ function renderIt(type, blob) {
 }
 
 function renderImage(blob) {
-	const ctx = getCanvasContext(true);
+	const ctx = getCanvasContext();
 	const img = new Image();
 	img.onload = (event) => {
 		ctx.drawImage(event.target, 0, 0);
@@ -41,25 +43,53 @@ function renderImage(blob) {
 		URL.revokeObjectURL(event.target.src);
 	};
 	img.src = URL.createObjectURL(blob);
+	ctx.translate(0, img.height);
+}
+
+function renderText(blob) {
+	const reader = new FileReader();
+
+	reader.addEventListener(
+		"load",
+		() => {
+			renderTextImpl(reader.result);
+		},
+		false,
+	);
+
+	reader.readAsText(blob);
+}
+
+function renderTextImpl(text) {
+	console.log(text);
+	const ctx = getCanvasContext();
+	ctx.fillStyle = "grey";
+	const { fontBoundingBoxAscent, fontBoundingBoxDescent } =
+		ctx.measureText(text);
+	ctx.fillText(text, 0, fontBoundingBoxAscent);
+	ctx.translate(0, fontBoundingBoxAscent + fontBoundingBoxDescent);
 }
 
 function getCanvasContext(clear = false) {
 	const canvas = document.getElementById("thecanvas"),
 		ctx = canvas.getContext("2d");
-	// Get the device pixel ratio, falling back to 1.
-	const dpr = window.devicePixelRatio || 1;
-	// Get the size of the canvas in CSS pixels.
-	const rect = canvas.getBoundingClientRect();
-	// Give the canvas pixel dimensions of their CSS
-	// size * the device pixel ratio.
-	canvas.width = rect.width * dpr;
-	canvas.height = rect.height * dpr;
-	canvas.style.width = rect.width+"px";
-	canvas.style.height = rect.height+"px";
-	// Scale all drawing operations by the dpr, so you
-	// don't have to worry about the difference.
-	ctx.scale(dpr, dpr);
 	if (clear) {
+		ctx.reset();
+		// Get the device pixel ratio, falling back to 1.
+		const dpr = window.devicePixelRatio || 1;
+		// Get the size of the canvas in CSS pixels.
+		const rect = canvas.getBoundingClientRect();
+		// Give the canvas pixel dimensions of their CSS
+		// size * the device pixel ratio.
+		canvas.width = rect.width * dpr;
+		canvas.height = rect.height * dpr;
+		// adjust style to match "original" size
+		canvas.style.width = rect.width + "px";
+		canvas.style.height = rect.height + "px";
+		// Scale all drawing operations by the dpr, so you
+		// don't have to worry about the difference.
+		ctx.scale(dpr, dpr);
+
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 	}
 	return ctx;
