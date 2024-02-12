5c8af0793439275bb0edbff2846ddd14fc91db35
Alex Rock <1675576+nynexman4464@users.noreply.github.com>
2024-02-12 16:47:11 -0500

    basic rendering of images

--- script.js
+++ script.js
@@ -1,19 +1,66 @@
-function paste(evt){
+function paste(evt) {
 	const x = navigator.clipboard.read();
-	x.then(handlePaste).catch(handleError)
+	x.then(handlePaste).catch(handleError);
 }
 
-async function handlePaste(items){
+async function handlePaste(items) {
 	console.log(items.length);
 	for (const item of items) {
 		for (const type of item.types) {
 			const blob = await item.getType(type);
-        // we can now use blob here
-			console.log(type, blob);
+			// we can now use blob here
+			renderIt(type, blob);
 		}
 	}
 }
 
-function handleError(err){
-	alert("oops: "+err);
+function handleError(err) {
+	alert("oops: " + err);
+}
+
+function renderIt(type, blob) {
+	console.log(type, blob);
+	const prefix = type.split("/");
+	switch (prefix[0]) {
+		case "image":
+			renderImage(blob);
+			break;
+		case "text":
+			break;
+		default:
+			handleError("Weird type: " + type);
+	}
+}
+
+function renderImage(blob) {
+	const ctx = getCanvasContext(true);
+	const img = new Image();
+	img.onload = (event) => {
+		ctx.drawImage(event.target, 0, 0);
+		// release blob after drawing
+		URL.revokeObjectURL(event.target.src);
+	};
+	img.src = URL.createObjectURL(blob);
+}
+
+function getCanvasContext(clear = false) {
+	const canvas = document.getElementById("thecanvas"),
+		ctx = canvas.getContext("2d");
+	// Get the device pixel ratio, falling back to 1.
+	const dpr = window.devicePixelRatio || 1;
+	// Get the size of the canvas in CSS pixels.
+	const rect = canvas.getBoundingClientRect();
+	// Give the canvas pixel dimensions of their CSS
+	// size * the device pixel ratio.
+	canvas.width = rect.width * dpr;
+	canvas.height = rect.height * dpr;
+	canvas.style.width = rect.width+"px";
+	canvas.style.height = rect.height+"px";
+	// Scale all drawing operations by the dpr, so you
+	// don't have to worry about the difference.
+	ctx.scale(dpr, dpr);
+	if (clear) {
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+	}
+	return ctx;
 }
